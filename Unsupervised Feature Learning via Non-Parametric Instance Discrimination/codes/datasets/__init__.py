99d69f65451e0a198620a690e0df250634e85098
yasu24120 <yasu.kobayashi1103@gmail.com>
2019-09-06 12:59:00 +0900

    Add files via upload

--- Unsupervised Feature Learning via Non-Parametric Instance Discrimination/codes/datasets/__init__.py
+++ Unsupervised Feature Learning via Non-Parametric Instance Discrimination/codes/datasets/__init__.py
@@ -1,6 +1,7 @@
 from .folder import ImageFolderInstance
 from .cifar import CIFAR10Instance, CIFAR100Instance
 from .mnist import MNISTInstance
+from .folder_custom import ImageFolder
 
-__all__ = ('ImageFolderInstance', 'MNISTInstance', 'CIFAR10Instance', 'CIFAR100Instance')
+__all__ = ('ImageFolderInstance', 'MNISTInstance', 'CIFAR10Instance', 'CIFAR100Instance', 'ImageFolder',  'DatasetFolder')
 
