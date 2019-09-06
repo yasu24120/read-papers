16b35c391a47fd71fd100c62527acb69a35b5f4f
yasu24120 <yasu.kobayashi1103@gmail.com>
2019-09-06 14:43:49 +0900

    Delete __init__.py

--- Unsupervised Feature Learning via Non-Parametric Instance Discrimination/codes/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .folder import ImageFolderInstance
-from .cifar import CIFAR10Instance, CIFAR100Instance
-from .mnist import MNISTInstance
-from .folder_custom import ImageFolder
-
-__all__ = ('ImageFolderInstance', 'MNISTInstance', 'CIFAR10Instance', 'CIFAR100Instance', 'ImageFolder',  'DatasetFolder')
-
